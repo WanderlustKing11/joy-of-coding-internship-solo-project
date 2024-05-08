a9c587d2df3b90c0b2c62a2321670626a8bd689b
Wande <wanderlustking11@gmail.com>
2024-05-08 14:27:00 -0400

    Create List template

--- app/listslibrary/list/page.tsx
+++ app/listslibrary/list/page.tsx
@@ -1,7 +1,66 @@
+import { DotsVerticalIcon, Pencil2Icon } from '@radix-ui/react-icons';
+import { Button, IconButton } from '@radix-ui/themes';
 import React from 'react';
 
 const ListPage = () => {
-  return <div>ListPage</div>;
+  const taskStyle =
+    'grid grid-cols-6 gap-4 py-2 items-center border-2 border-solid border-zinc-500 rounded-xl border-b last:border-b-1 hover:bg-zinc-400';
+
+  return (
+    <div className='w-full h-full flex flex-col items-center'>
+      {/* TITLE */}
+      <div className='flex flex-row space-x-10 group'>
+        <h1 className='text-3xl mb-14 group-hover:opacity-75'>List Title</h1>
+        <span className='opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
+          <IconButton size='1' aria-label='Edit list title'>
+            <Pencil2Icon width='18' height='18' />
+          </IconButton>
+        </span>
+      </div>
+
+      <div className='w-full'>
+        <ul>
+          <li className={taskStyle}>
+            <div className='pl-4 col-span-2 truncate ...'>
+              Task #1 blah blah blah blah blah blah blah blah blah
+            </div>
+            <div className='col-start-4 col-span-2'>due: 5/10/24</div>
+            <IconButton
+              className='col-start-6 flex justify-center'
+              color='gray'
+              variant='soft'
+            >
+              <DotsVerticalIcon width='18' height='18' />
+            </IconButton>
+          </li>
+          <li className={taskStyle}>
+            <div className='pl-4 col-span-2 truncate ...'>Task #2</div>
+            <div className='col-start-4 col-span-2'>due: 5/10/24</div>
+            <IconButton className='col-start-6' color='gray' variant='soft'>
+              <DotsVerticalIcon width='18' height='18' />
+            </IconButton>
+          </li>
+          <li className={taskStyle}>
+            <div className='pl-4 col-span-2 truncate ...'>Task #3</div>
+            <div className='col-start-4 col-span-2'>due: 5/10/24</div>
+            <IconButton className='col-start-6' color='gray' variant='soft'>
+              <DotsVerticalIcon width='18' height='18' />
+            </IconButton>
+          </li>
+          <li className={taskStyle}>
+            <div className='pl-4 col-span-2 truncate ...'>Task #4</div>
+            <div className='col-start-4 col-span-2'>due: 5/10/24</div>
+            <IconButton className='col-start-6' color='gray' variant='soft'>
+              <DotsVerticalIcon width='18' height='18' />
+            </IconButton>
+          </li>
+        </ul>
+      </div>
+      <div className='mt-16 mb-10'>
+        <Button>ADD NEW TASK</Button>
+      </div>
+    </div>
+  );
 };
 
 export default ListPage;
